86260f6116214227f3ba0403f7f45592954c1eb8
s1mpleow <dung.dq@geekup.vn>
2024-04-05 10:51:17 +0700

    Add systemd script to start PostgreSQL-Python service

---
+++ systemd_jenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+APP_NAME="postgresql-python"
+APP_PORT="8080"
+PROCESS_NAME="${APP_NAME}.service"
+
+sudo cp "${APP_NAME}.service" /lib/systemd/system/
+
+sudo chmod +x "/lib/systemd/system/${APP_NAME}.service"
+
+sudo systemctl daemon-reload
+
+sudo fuser -k "${APP_PORT}/tcp" || true
+
+sudo systemctl start "${PROCESS_NAME}"
+
+sleep 10
+
+sudo systemctl status "${PROCESS_NAME}"
